98da8041cde66f56bcc98e2673d45d4d845a9aa3
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-06-16 00:31:59 +0400

    bug fix

--- rtl/stage_controller.sv
+++ rtl/stage_controller.sv
@@ -8,7 +8,12 @@ module stage_controller (
         flush_vector = 4'b0000;
         stall_vector = 4'b0000;
         if (i_misprediction) begin
-            flush_vector = 4'b1111;  // flush everything (we dont actully need to flush fetch here as it flushes its outputs upon misprediction but anyways!)
+            /*
+            flush everything except execution stage
+            upon misprediction execution flushes itself already (so does the fetch by using signal exi.redirect)
+            jump instructions also trigger flushing, we cant flush them as they write to registers
+            */
+            flush_vector = 4'b1110;
             stall_vector = 3'b0000;
         end else if (i_load_stall) begin
             flush_vector = 4'b0001;  // flush execute stage
