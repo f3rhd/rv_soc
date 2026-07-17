3fa350d749cc3ebf4ddde4d327869c909e5e5424
Farhad <178732550+f3rhd@users.noreply.github.com>
2026-07-17 23:31:45 +0400

    change from unsigned to signed

--- libc-baremetal/include/st7735.h
+++ libc-baremetal/include/st7735.h
@@ -1,42 +1,42 @@
 #ifndef ST7735_H
 #define ST7735_H
 #include "st7735_colors.h"
-void st7735_stream_pixel(unsigned color, unsigned amount);
+void st7735_stream_pixel(unsigned color, int amount);
 void st7735_set_rectangle(
-    unsigned x_start,
-    unsigned y_start,
-    unsigned x_end,
-    unsigned y_end
+    int x_start,
+    int y_start,
+    int x_end,
+    int y_end
 );
-void st7735_draw_pixel(unsigned color, unsigned x, unsigned y);
+void st7735_draw_pixel(unsigned color, int x, int y);
 void st7735_draw_rectangle(
-    unsigned color,
-    unsigned x_start,
-    unsigned y_start,
-    unsigned x_end,
-    unsigned y_end
+    int x_start,
+    int y_start,
+    int x_end,
+    int y_end,
+    unsigned color
 );
 void st7735_draw_triangle(
     unsigned color,
-    unsigned p1_x,
-    unsigned p1_y,
-    unsigned p2_x,
-    unsigned p2_y,
-    unsigned p3_x,
-    unsigned p3_y
+    int p1_x,
+    int p1_y,
+    int p2_x,
+    int p2_y,
+    int p3_x,
+    int p3_y
 );
 void st7735_draw_line(
     unsigned color,
-    unsigned x_start,
-    unsigned y_start,
-    unsigned x_end,
-    unsigned y_end
+    int x_start,
+    int y_start,
+    int x_end,
+    int y_end
 );
 void st7735_draw_circle(
     unsigned color,
-    unsigned center_x,
-    unsigned center_y,
-    unsigned radius
+    int center_x,
+    int center_y,
+    int radius
 );
 
 #endif
--- libc-baremetal/src/st7735.c
+++ libc-baremetal/src/st7735.c
@@ -1,42 +1,44 @@
 #include "../include/st7735.h"
-inline void st7735_stream_pixel(unsigned color, unsigned amount);
+inline void st7735_stream_pixel(unsigned int color, int amount);
 inline void st7735_set_rectangle(
-    unsigned x_start,
-    unsigned y_start,
-    unsigned x_end,
-    unsigned y_end
+    int x_start,
+    int y_start,
+    int x_end,
+    int y_end
 );
-inline void st7735_draw_pixel(unsigned color, unsigned x, unsigned y) {
+inline void st7735_draw_pixel(unsigned int color, int x, int y) {
     st7735_set_rectangle(x, y, x, y);
     st7735_stream_pixel(color, 1);
 }
 inline void st7735_draw_rectangle(
-    unsigned x_start,
-    unsigned x_end,
-    unsigned y_start,
-    unsigned y_end,
-    unsigned color
+    int x_start,
+    int x_end,
+    int y_start,
+    int y_end,
+    unsigned int color
 ) {
     st7735_set_rectangle(x_start, x_end, y_start, y_end);
     // each color data corresponds to two pixel values
     st7735_stream_pixel(color, (x_end - x_start) * (y_end - y_start) >> 1);
 }
-// TODO 
+// TODO
 void st7735_draw_triangle(
-    unsigned color,
-    unsigned p1_x,
-    unsigned p1_y,
-    unsigned p2_x,
-    unsigned p2_y,
-    unsigned p3_x,
-    unsigned p3_y
-) {}
+    unsigned int color,
+    int p1_x,
+    int p1_y,
+    int p2_x,
+    int p2_y,
+    int p3_x,
+    int p3_y
+) {
+
+}
 void st7735_draw_line(
-    unsigned color,
-    unsigned x_start,
-    unsigned y_start,
-    unsigned x_end,
-    unsigned y_end
+    unsigned int color,
+    int x_start,
+    int y_start,
+    int x_end,
+    int y_end
 ) {
 
     //Bresenham's line algorithm
@@ -58,7 +60,7 @@ void st7735_draw_line(
     int err = dx + dy;
 
     while (1) {
-        st7735_draw_pixel(color, (unsigned)x0, (unsigned)y0);
+        st7735_draw_pixel(color, (int)x0, (int)y0);
 
         if (x0 == x1 && y0 == y1)
             break;
@@ -80,13 +82,13 @@ void st7735_draw_line(
 // TODO 
 void st7735_draw_circle(
     unsigned int color,
-    unsigned int center_x,
-    unsigned int center_y,
-    unsigned int radius
+    int center_x,
+    int center_y,
+    int radius
 ) {}
 
-inline void st7735_stream_pixel(unsigned color, unsigned amount) {
-    volatile unsigned* data_address = (volatile unsigned*)0xFFFFFFFF;
+inline void st7735_stream_pixel(unsigned int color, int amount) {
+    volatile int* data_address = (volatile int*)0xFFFFFFFF;
 
     for (int i = 0; i < amount; i++) {
         asm volatile("sw %0, 0(%1) \n\t"
@@ -97,17 +99,17 @@ inline void st7735_stream_pixel(unsigned color, unsigned amount) {
 }
 
 inline void st7735_set_rectangle(
-    unsigned x_start,
-    unsigned y_start,
-    unsigned x_end,
-    unsigned y_end
+    int x_start,
+    int y_start,
+    int x_end,
+    int y_end
 ) {
-    volatile unsigned* command_address = (volatile unsigned*)0xFFFFFFF0;
-    unsigned stream_cmd = 0x2C000000;
+    volatile int* command_address = (volatile int*)0xFFFFFFF0;
+    int stream_cmd = 0x2C000000;
 
-    unsigned col_value =
+    int col_value =
         (0x2A << 24) | ((x_start & 0xFF) << 16) | ((x_end & 0xFF) << 8);
-    unsigned row_value =
+    int row_value =
         (0x2B << 24) | ((y_start & 0xFF) << 16) | ((y_end & 0xFF) << 8);
 
     asm volatile("sw %0, 0(%2) \n\t"
